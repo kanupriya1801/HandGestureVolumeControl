9ff081a9b1ca634cad8eff5ffdc721e97bfb8af6
Kanupriya <115139535+kanupriya1801@users.noreply.github.com>
2025-03-20 12:24:22 +0530

    Update VolumeHandControlAdvance.py

--- VolumeHandControlAdvance.py
+++ VolumeHandControlAdvance.py
@@ -7,7 +7,7 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 #####################################
 wCam, hCam = 640, 480
-#####################################
+
 
 cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
